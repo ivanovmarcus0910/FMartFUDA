42e7335d6c5ed5e64c6dd75317e77f6f2b1e9d93
Anh Thợ Code <113629275+ivanovmarcus0910@users.noreply.github.com>
2025-03-28 09:26:22 +0700

    Add Employee DAO + Repository

---
+++ Repositories/EmployeeRepository.cs
@@ -0,0 +1,37 @@
+﻿using Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess;
+namespace Repositories
+{
+    public class EmployeeRepository : IFormRepositories<Employee>
+    {
+        public async Task<IEnumerable<Employee>> GetAllAsync()
+        {
+            return await EmployeeDAO.Instance.GetAllAsync(); // Correct: await the async method
+        }
+
+        public async Task<Employee> GetByIdAsync(int id)
+        {
+            return await EmployeeDAO.Instance.GetByIdAsync(id); // Correct: await the async method
+        }
+
+        public async Task<Employee> AddAsync(Employee entity)
+        {
+            return await EmployeeDAO.Instance.AddAsync(entity); // Call the base AddAsync
+        }
+
+        public async Task<Employee> UpdateAsync(Employee entity)
+        {
+            return await EmployeeDAO.Instance.UpdateAsync(entity); // Correct: await and return
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            return await EmployeeDAO.Instance.DeleteAsync(id); // Correct: await and return
+        }
+    }
+}
