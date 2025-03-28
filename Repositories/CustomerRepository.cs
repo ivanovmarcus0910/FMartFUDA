a992f467d1bb5a80a50b237d6a3a20e2891a1d93
TRUNG <trinhquoctrung10504@gmail.com>
2025-03-28 09:52:16 +0700

    Thêm customerDAO và customerRepository

---
+++ Repositories/CustomerRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess;
+using Models.Models;
+
+namespace Repositories
+{
+    public class CustomerRepository : IFormRepositories<Customer>
+    {
+        public async Task<IEnumerable<Customer>> GetAllAsync()
+        {
+            return await CustomerDAO.Instance.GetAllAsync(); // Correct: await the async method
+        }
+
+        public async Task<Customer> GetByIdAsync(int id)
+        {
+            return await CustomerDAO.Instance.GetByIdAsync(id); // Correct: await the async method
+        }
+
+        public async Task<Customer> AddAsync(Customer entity)
+        {
+            return await CustomerDAO.Instance.AddAsync(entity); // Call the base AddAsync
+        }
+
+        public async Task<Customer> UpdateAsync(Customer entity)
+        {
+            return await CustomerDAO.Instance.UpdateAsync(entity); // Correct: await and return
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            return await CustomerDAO.Instance.DeleteAsync(id); // Correct: await and return
+        }
+    }
+}
