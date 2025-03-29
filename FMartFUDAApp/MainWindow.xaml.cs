2f60f0ff71b0bcdfd3e360c796413ad625beed96
hai <hainlde180364@fpt.edu.vn>
2025-03-29 09:28:46 +0700

    giao diện

--- FMartFUDAApp/MainWindow.xaml.cs
+++ FMartFUDAApp/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace FMartFUDAApp
 {
@@ -18,10 +19,15 @@ namespace FMartFUDAApp
     public partial class MainWindow : Window
     {
         Employee employeeCurrent;
+        private DispatcherTimer timer;
         public MainWindow(Employee employee)
+             
+
         {
             InitializeComponent();
             employeeCurrent = employee;
+            welcomeTextBlock.Text = $"Welcome, {employee.EmployeeName}";
+            StartTimer();
         }
 
       
@@ -70,6 +76,38 @@ namespace FMartFUDAApp
             FinancialManagement next = new FinancialManagement(employeeCurrent);
             next.Show();
             Close();
+        }
+        private void Logoutbtn(object sender, RoutedEventArgs e)
+        {
+            var result = MessageBox.Show(
+                "Are you sure you want to log out?",
+                "Logout Confirmation",
+                MessageBoxButton.YesNo,
+                MessageBoxImage.Question
+            );
+
+            if (result == MessageBoxResult.Yes)
+            {
+                RedirectToLogin();
+            }
+        }
+        private void RedirectToLogin()
+        {
+            Login loginWindow = new Login();
+            loginWindow.Show();
+            this.Close(); // Đóng cửa sổ hiện tại
+        }
+        private void StartTimer()
+        {
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1); // Cập nhật mỗi giây
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            txtClock.Text = DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss");
         }
 
 
