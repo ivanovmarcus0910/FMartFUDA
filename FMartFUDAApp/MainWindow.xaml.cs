d42337c5dca855a7602a3233ac8b085f4bb9e6e8
Anh Thợ Code <113629275+ivanovmarcus0910@users.noreply.github.com>
2025-03-30 19:02:45 +0700

    Change Style

--- FMartFUDAApp/MainWindow.xaml.cs
+++ FMartFUDAApp/MainWindow.xaml.cs
@@ -6,6 +6,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -30,9 +31,7 @@ namespace FMartFUDAApp
             StartTimer();
         }
 
-      
 
-        
         private void CustomerManagement(object sender, RoutedEventArgs e)
         {
             MainFrame.Navigate(new Customer_Manager(employeeCurrent));
